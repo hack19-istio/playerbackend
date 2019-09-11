7ca537050eff728aee32e3b8aeef16ca3ace71b7
Joerg Wuethrich <joerg.wuethrich@axa-winterthur.ch>
2019-09-11 11:20:17 +0200

    read server_port and /instrument url from env variables

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require('express')
 const http = require('http');
 const fs = require('fs');
 const app = express();
-const port = 3000;
 
 app.get('/instrument', async (req, res) => {
   console.log('/instrument: calling', req.query.name);
-  const response = await callInstrumentService('http://localhost:8080/instrument', req.query.name);
+  const response = await callInstrumentService(getInstrumentServiceUrl(), req.query.name);
   return res.send(`Hello ${response}!`)
 })
 
@@ -16,7 +15,13 @@ app.get('/instrument-file', async (req, res) => {
   return res.send(instrumentFile);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.server_port, () => console.log(`listening on port ${process.env.server_port}!`))
+
+const getInstrumentServiceUrl = () => {
+  const url = `${process.env.instrument_host}:${process.env.instrument_port}/${process.env.instrument_resource}`;
+  console.log('instrument service url', url);
+  return url;
+}
 
 const callInstrumentService = (url) => {
   return new Promise((resolve, reject) => http.get(url, (res) => {
