092aea7a1a20667d820a6ead713950e9f60c8dc4
Joerg Wuethrich <joerg.wuethrich@axa-winterthur.ch>
2019-09-11 10:36:42 +0200

    implement dummy call to instrument microservice

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const http = require('http');
 const app = express()
 const port = 3000
 
-app.get('/instrument', (req, res) => {
-  return res.send(`Hello ${req.query.name}!`)
+app.get('/instrument', async (req, res) => {
+  const response = await callService('http://localhost:8080/instrument', req.query.name);
+  return res.send(`Hello ${response}!`)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const callService = (url) => {
+  return new Promise((resolve, reject) => http.get(url, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+
+    res.on('data', (d) => {
+      resolve(d);
+    });
+
+  }).on('error', (e) => {
+    console.error(e);
+    reject(e);
+  }));
+}
